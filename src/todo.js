515fe26b4e9c2255a088fd8115711d2310479974
yhorie <yuk.horie@gmail.com>
2019-05-02 10:23:06 +0900

    Add Change todo feature

--- src/todo.js
+++ src/todo.js
@@ -1,5 +1,26 @@
 /* global Vue */
 Vue.component('todo-item', {
   props: ['todo'],
-  template: '<li>{{ todo.id }} - {{ todo.text }}</li>'
+  template: `<li v-bind:class="{ editing: editing, readOnly: readOnly } "v-on:dblclick="changeState()">
+               <span>{{ todo.text }}</span>
+               <input v-on:blur="changeState()" v-on:keyup.enter="changeVal()" type="text" v-model="todo.text" autofocus>
+             </li>`,
+  data: function(){
+    return {
+      editing: false,
+      readOnly: true
+    };
+  },
+  methods: {
+    changeState: function(){
+      this.$data.editing = !this.editing;
+      this.$data.readOnly = !this.readOnly;
+      if(this.editing){
+        this.$nextTick(() => this.$el.getElementsByTagName('input')[0].focus());
+      }
+    },
+    changeVal: function(){
+      this.$el.children[1].blur();
+    }
+  },
 });
